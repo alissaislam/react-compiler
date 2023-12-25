4ed9e3402b739f0283b3a821f927cfb2f1967c44
Ahmad TN <support@ahtcloud.com>
2023-12-25 14:28:10 +0300

    salam alekom

--- src/ast/Models/CallFunction.java
+++ src/ast/Models/CallFunction.java
@@ -1,4 +1,10 @@
 package ast.Models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CallFunction extends Node{
+    List<SimpleCallfunction> simpleCallfunctions= new ArrayList<>();
+    List<CallIdentifier> callIdentifiers = new ArrayList<>();
+
 }
